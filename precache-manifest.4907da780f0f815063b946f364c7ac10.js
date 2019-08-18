5a985f8c4835fde69baecd4f264afef6cc1b5156
Alina Ray <a.alina.ray@gmail.com>
2019-08-19 00:09:54 +0300

    Updates

---
+++ precache-manifest.4907da780f0f815063b946f364c7ac10.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ec8beb192d0ac3545d59bed053a8dab",
+    "url": "/star-db/index.html"
+  },
+  {
+    "revision": "7f06a8424d8b3e65c6bc",
+    "url": "/star-db/static/css/main.5bf8e34f.chunk.css"
+  },
+  {
+    "revision": "55736523605263389c7e",
+    "url": "/star-db/static/js/2.97a166df.chunk.js"
+  },
+  {
+    "revision": "7f06a8424d8b3e65c6bc",
+    "url": "/star-db/static/js/main.ea24edbf.chunk.js"
+  },
+  {
+    "revision": "ed5d1bbf11aa36b8ad39",
+    "url": "/star-db/static/js/runtime~main.e24185d8.js"
+  }
+]);
